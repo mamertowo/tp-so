ab781c0e2d64e37120bc2938706149abd0e29506
Valentino Murga <valenmurga23@gmail.com>
2024-09-29 17:54:49 -0300

    primer commit

---
+++ codigo/src/HashMapConcurrente.cpp
@@ -0,0 +1,50 @@
+#ifndef CHM_CPP
+#define CHM_CPP
+
+#include <thread>
+#include <iostream>
+#include <fstream>
+
+#include "HashMapConcurrente.hpp"
+
+HashMapConcurrente::HashMapConcurrente() {
+    for (unsigned int i = 0; i < HashMapConcurrente::cantLetras; i++) {
+        tabla[i] = new ListaAtomica<hashMapPair>();
+    }
+}
+
+unsigned int HashMapConcurrente::hashIndex(std::string clave) {
+    return (unsigned int)(clave[0] - 'a');
+}
+
+void HashMapConcurrente::incrementar(std::string clave) {
+    // Completar (Ejercicio 2)
+}
+
+std::vector<std::string> HashMapConcurrente::claves() {
+    // Completar (Ejercicio 2)
+}
+
+unsigned int HashMapConcurrente::valor(std::string clave) {
+    // Completar (Ejercicio 2)
+}
+
+float HashMapConcurrente::promedio() {
+
+    float sum = 0.0;
+    unsigned int count = 0;
+    for (unsigned int index = 0; index < HashMapConcurrente::cantLetras; index++) {
+        for (const auto& p : *tabla[index]) {
+            sum += p.second;
+            count++;
+        }
+    }
+    if (count > 0) {
+        return sum / count;
+    }
+    return 0;        
+}
+
+
+
+#endif
