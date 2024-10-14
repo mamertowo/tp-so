65371ba45ad3e66bdfee04c1a424396fd16fdf1a
Valentino Murga <valenmurga23@gmail.com>
2024-10-13 21:34:18 -0300

    cambios en concurrencia

--- codigo/src/HashMapConcurrente.cpp
+++ codigo/src/HashMapConcurrente.cpp
@@ -22,7 +22,7 @@ unsigned int HashMapConcurrente::hashIndex(std::string clave) {
 void HashMapConcurrente::incrementar(std::string clave) {
     int letra = hashIndex(clave);
     bucketMtx[letra]->lock();
-    for (hashMapPair &par : *tabla[letra]) {        // El iterador se rompe si se llegara a agregar un elemento en el medio
+    for (hashMapPair &par : *tabla[letra]) {        // No se rompen los iteradores
         if (par.first == clave) {
             par.second++;
             bucketMtx[letra]->unlock();
@@ -35,10 +35,7 @@ void HashMapConcurrente::incrementar(std::string clave) {
 
 std::vector<std::string> HashMapConcurrente::claves() {
     std::vector<std::string> res;
-    // Que pasa si alguien mete una clave nueva en el medio
-    // mtx[todos].lock?
     for (int i = 0; i < HashMapConcurrente::cantLetras; i++) {
-        // mtx[i].lock?
         for (hashMapPair par : *tabla[i]) {
             res.push_back(par.first);
         }
@@ -48,7 +45,6 @@ std::vector<std::string> HashMapConcurrente::claves() {
 
 unsigned int HashMapConcurrente::valor(std::string clave) {
     int letra = hashIndex(clave);
-    // mismo
     for (hashMapPair par : *tabla[letra]) {
         if (par.first == clave) {
             return par.second;
@@ -60,14 +56,13 @@ unsigned int HashMapConcurrente::valor(std::string clave) {
 float HashMapConcurrente::promedio() {
     float sum = 0.0;
     unsigned int count = 0;
-    // Trabamos todos de una y vamos destrabando de a uno
-    for (int i = 0; i < HashMapConcurrente::cantLetras; i++) bucketMtx[i]->lock();
     for (unsigned int index = 0; index < HashMapConcurrente::cantLetras; index++) {
+        // bucketMtx[index]->lock();
         for (const auto& p : *tabla[index]) {
             sum += p.second;
             count++;
         }
-        bucketMtx[index]->unlock();
+        // bucketMtx[index]->unlock();
     }
     if (count > 0) {
         return sum / count;
@@ -81,21 +76,20 @@ float HashMapConcurrente::promedioParalelo(unsigned int cantThreads) {
     std::atomic_int count(0);
     std::atomic_int nextLetra(0);
 
-    for (int i = 0; i < HashMapConcurrente::cantLetras; i++) bucketMtx[i]->lock();
-
     std::thread threads[cantThreads];
     for (unsigned int i = 0; i < cantThreads; i++) {
         // errores de creacion de thread
         threads[i] = std::thread([&sum, &mtxSum, &count, &nextLetra, *this] () {
             int letra;
             while ((letra = (nextLetra++)) < HashMapConcurrente::cantLetras) {
+                // bucketMtx[index]->lock();
                 for (const hashMapPair& par : *tabla[letra]) {
                     mtxSum.lock();
                     sum += par.second;
                     mtxSum.unlock();
                     count++;
                 }
-                bucketMtx[letra]->unlock();
+                // bucketMtx[letra]->unlock();
             }
         });
     }
